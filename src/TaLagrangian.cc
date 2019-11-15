4bf925d233ef21cc5f94096c8c2f4c740d00ffae
Tao Ye <tao.ye@stonybrook.edu>
2019-11-15 00:44:28 -0500

    Debuging lagrange

--- src/TaLagrangian.cc
+++ src/TaLagrangian.cc
@@ -17,16 +17,20 @@ void TaLagrangian::LoadConstraint(Int_t ana_index, TaConfig *aConfig){
 #ifdef NOISY
   cout << __PRETTY_FUNCTION__ << endl;
 #endif
-  TFile ext_file(aConfig->GetAnalysisParameter(ana_index,"sens_input"));
+  TString ext_filename = aConfig->GetAnalysisParameter(ana_index,"sens_input");
+  TFile ext_file(ext_filename);
+  if(!ext_file.IsOpen()){
+    cout << ext_filename <<" doesn't exist!" << endl;
+    return;
+  }
   vector<TString> sCoilList = aConfig->GetCoilList(ana_index);
   
   vector<TString> *ext_dv_array =
     (vector<TString>*)ext_file.Get("dv_array");
   vector<TString> *ext_coil_array =
     (vector<TString>*)ext_file.Get("coil_array");
-  
+
   TMatrixD* sens_matrix = (TMatrixD*) ext_file.Get("sens_matrix"); 
-  
   nCoil = sCoilList.size();
   Int_t nDet = sDVlist.size();
   Int_t nMon = sIVlist.size();
@@ -36,11 +40,15 @@ void TaLagrangian::LoadConstraint(Int_t ana_index, TaConfig *aConfig){
 
   for(int icoil=0;icoil<nCoil;icoil++){
     auto iter_coil = find((*ext_coil_array).begin(),(*ext_coil_array).end(),
-			  GetBaseName(sCoilList[icoil]));
+			  sCoilList[icoil]);
+    if(iter_coil==(*ext_coil_array).end())
+      cerr<< sCoilList[icoil] << " not found in matrix files." << endl;
     Int_t index_coil = iter_coil-(*ext_coil_array).begin();
     for(int idet=0;idet<nDet;idet++){
       auto iter_det = find((*ext_dv_array).begin(),(*ext_dv_array).end(),
 			   GetBaseName(sDVlist[idet]));
+      if(iter_det==(*ext_dv_array).end())
+	cerr<< sDVlist[idet] << " not found in matrix files." << endl;
       Int_t index_det = iter_det-(*ext_dv_array).begin();
       detConstraints(icoil,idet)=(*sens_matrix)[index_det][index_coil];
     }
@@ -48,11 +56,13 @@ void TaLagrangian::LoadConstraint(Int_t ana_index, TaConfig *aConfig){
     for(int imon=0;imon<nMon;imon++){
       auto iter_mon = find((*ext_dv_array).begin(),(*ext_dv_array).end(),
 			   GetBaseName(sIVlist[imon]));
+      if(iter_mon==(*ext_dv_array).end())
+	cerr<< sIVlist[imon] << " not found in matrix files." << endl;
       Int_t index_mon = iter_mon-(*ext_dv_array).begin();
       monConstraints(icoil,imon)=(*sens_matrix)[index_mon][index_coil];
     }
   }
-#ifdef DEBUG 
+#ifdef NOISY 
   monConstraints.Print();
   detConstraints.Print();
 #endif  
@@ -67,7 +77,7 @@ TMatrixD TaLagrangian::Solve(TMatrixD CovDM, TMatrixD CovMM){
   Int_t nDet = CovDM.GetNcols();
   TMatrixD lhsM(nMon+nCoil,nMon+nCoil);    
   TMatrixD rhsM(nMon+nCoil,nDet);    
-  TMatrixD monConstraintsTran;
+  TMatrixD monConstraintsTran(nMon,nCoil);
   monConstraintsTran.Transpose(monConstraints);
   lhsM.SetSub(0,nMon,monConstraintsTran);
   lhsM.SetSub(nMon,0,monConstraints);
@@ -77,10 +87,10 @@ TMatrixD TaLagrangian::Solve(TMatrixD CovDM, TMatrixD CovMM){
 
   TMatrixD invlhsM = lhsM.Invert();
   TMatrixD solutionM = invlhsM*rhsM;
-#ifdef NOISY	
-  cout << " -- Slopes Matrix (nMon x nDet) " << endl;
   TMatrixD slopeM(nMon,nDet);
   slopeM=solutionM.GetSub(0,nMon-1,0,nDet-1);
+#ifdef NOISY	
+  cout << " -- Slopes Matrix (nMon x nDet) " << endl;
   slopeM.Print();
 #endif
 
@@ -95,4 +105,5 @@ TMatrixD TaLagrangian::Solve(TMatrixD CovDM, TMatrixD CovMM){
 TString TaLagrangian::GetBaseName(TString in){
   in.ReplaceAll("diff_","");
   in.ReplaceAll("asym_","");
+  return in;
 }
