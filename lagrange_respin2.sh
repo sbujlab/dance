21f759d786fa8be3eed7cd0f80fd4889ef47e0ca
Tao Ye <tao.ye@stonybrook.edu>
2020-06-22 01:07:11 -0400

    respin2 shell/ifarm scripts

---
+++ lagrange_respin2.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+runnum=$1;
+
+if [ -z "$runnum" ] 
+then
+    echo "Run Number is empty";
+    exit 1;
+fi    
+
+level="Prompt"
+shopt -s extglob
+# find split file
+rootfile_list=$(ls -1 /lustre/expphy/volatile/halla/parity/prex-respin2/japanOutput/prex$level\_pass2_$runnum.!(*farm*).root);
+shopt -u extglob
+
+for rootfile  in $rootfile_list
+do
+    trim=${rootfile%.root}
+    run_dot_seg=${trim#*pass2_}
+    run_num=${run_dot_seg%.*}
+    run_seg=${run_dot_seg/./_}
+
+    echo $(($run_num))
+    echo $run_num
+
+    conf="lagrange-respin2.conf"
+    if [ $(($run_num)) -ge 3390 ]
+    then
+      conf="lagrange-respin2.3390-.conf"
+    fi
+    if [ $(($run_num)) -ge 4735 ]
+    then
+      conf="lagrange-respin2.4735-.conf"
+    fi
+
+    ./dance \
+    	-f $rootfile \
+    	-c ./conf/$conf ; 
+done
+
---
+++ submit_lagrange_respin2.py
@@ -0,0 +1,74 @@
+#!/apps/python/PRO/bin/python
+from subprocess import call
+import subprocess
+import sys,os,time
+
+def main():
+    
+    _email="tao@jlab.org"
+    _mssdir="/mss/halla/parity/raw"
+    _source="/u/group/halla/parity/software/japan_offline/lagrange/"
+    _directory="/lustre/expphy/cache/halla/parity/raw"
+    _rootout="./rootfiles/"
+    _nrStart=2000
+    _nrStop=6000
+    submit=1
+    useSWIF=1 #0: uses jsub 1: uses SWIF+jsub
+
+    firstrun=9999
+    lastrun=0
+    _runlist=[]
+    runfile=open(_source+"/prex-runlist/simple_list/slug1.list","r")
+    for line in runfile:
+        _runlist.append(int(line))
+        if (firstrun >= int(line) and _nrStart <= int(line)):
+            firstrun=int(line)
+        if (lastrun <= int(line) and _nrStop >= int(line)):
+            lastrun=int(line)
+    runfile.close()
+                
+    _workflowID="Lagrange_"+str(firstrun)+"_"+str(lastrun)
+
+    createXMLfile(_mssdir,_source,_rootout,_nrStart,_nrStop,_email,_workflowID,_runlist)
+
+    if submit==1:
+        if useSWIF==1:
+            print "Submitting lagrange prompt analysis for runs "+str(firstrun)+" to "+str(lastrun)+" using designated SWIF workflow "+str(_workflowID)
+            call(["swif","add-jsub","-workflow",str(_workflowID),"-create","-script",_source+"/"+_workflowID+".xml"])
+        elif useSWIF==0:
+            print "submitting position sampled with id between ",firstrun,lastrun
+            call(["jsub","-xml",_source+"/"+_workflowID+".xml"])
+        else:
+            print "NOT submitting position sampled with id between ",firstrun,lastrun
+            
+    print "I am all done"
+
+
+def createXMLfile(mssdir,source,rootout,nStart,nStop,email,workflowID,runlist):
+
+    f=open(source+"/"+workflowID+".xml","w")
+    f.write("<Request>\n")
+    f.write("  <Email email=\""+email+"\" request=\"false\" job=\"true\"/>\n")
+    f.write("  <Project name=\"prex\"/>\n")
+    f.write("  <Track name=\"one_pass\"/>\n")
+    f.write("  <Name name=\""+workflowID+"\"/>\n")
+    f.write("  <OS name=\"centos7\"/>\n")
+    f.write("  <Memory space=\"2000\" unit=\"MB\"/>\n")
+    for nr in runlist:
+        if (nr < nStart or nr > nStop):
+            continue
+        f.write("  <Job>\n")
+        f.write("    <Command><![CDATA[\n")
+        f.write("    cd "+source+"\n")
+        f.write("    "+source+"/lagrange_respin2.sh "+str(nr)+"; \n")
+        f.write("    ]]></Command>\n")
+        f.write("    <Stdout dest=\""+source+"/LogFiles/lagrange_respin2log"+"_%04d"%(nr)+".out\"/>\n")
+        f.write("    <Stderr dest=\""+source+"/LogFiles/lagrange_respin2log"+"_%04d"%(nr)+".err\"/>\n")
+        f.write("  </Job>\n\n")
+
+    f.write("</Request>\n")
+    f.close()
+    return 0
+                    
+if __name__ == '__main__':
+    main()
